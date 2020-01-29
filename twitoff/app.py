90134c47479c9068da3a2cacb6c7c089cb0634eb
Alexander Kim <alex000kim@gmail.com>
2020-01-29 10:57:30 -0500

    init

---
+++ twitoff/app.py
@@ -0,0 +1,25 @@
+from decouple import config
+from flask import Flask, render_template
+
+from .models import DB, User
+
+
+def create_app():
+    """Create and configure an instance of the Flask application"""
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        DB.create_all()
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
+    return app
