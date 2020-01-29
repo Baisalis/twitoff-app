913c5662e2ca24308acb3008028fcd97c9e6da28
Alexander Kim <alex000kim@gmail.com>
2020-01-29 13:43:19 -0500

    implement functionality to add new users; add user.html template

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,7 +1,8 @@
 from decouple import config
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from .models import DB, User
+from .twitter import add_or_update_user
 
 
 def create_app():
@@ -15,6 +16,20 @@ def create_app():
     def root():
         DB.create_all()
         return render_template('base.html', title='Home', users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['username']
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = f'User {name} successfully added!'
+            tweets = User.query.filter(User.name==name).one().tweets
+        except Exception as e:
+            message = f'Error while try to add user {name}: {e}'
+            tweets = []
+        return render_template('user.html', title=name, message=message, tweets=tweets)
 
     @app.route('/reset')
     def reset():
