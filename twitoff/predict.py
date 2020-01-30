0df435825d98645fdc1667c0af254aaaa0a60b1b
Alexander Kim <alex000kim@gmail.com>
2020-01-30 13:40:29 -0500

    fix typo

--- twitoff/predict.py
+++ twitoff/predict.py
@@ -12,7 +12,7 @@ def predict_user(user1_name, user2_name, tweet_text):
     user1 = User.query.filter(User.name == user1_name).one()
     user2 = User.query.filter(User.name == user2_name).one()
     user1_embeddings = np.array([tweet.embedding for tweet in user1.tweets])
-    user2 _embeddings = np.array([tweet.embedding for tweet in user2.tweets])
+    user2_embeddings = np.array([tweet.embedding for tweet in user2.tweets])
     embeddings = np.vstack([user1_embeddings, user2_embeddings])
     labels = np.concatenate([np.ones(len(user1.tweets)),
                              np.zeros(len(user2.tweets))])
